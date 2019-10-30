feeb8a2cab83d04358838e146c53574a4bee7d42
Nicola Piga <nicolapiga@gmail.com>
2019-10-30 19:18:05 +0100

    Remove unnecessary color conversion in iCubCAmera::get_rgb()

--- src/camera-library/src/iCubCamera.cpp
+++ src/camera-library/src/iCubCamera.cpp
@@ -170,7 +170,6 @@ std::pair<bool, cv::Mat> iCubCamera::get_rgb(const bool& blocking)
         return std::make_pair(false, cv::Mat());
 
     cv::Mat image = yarp::cv::toCvMat(*image_in);
-    cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
 
     return std::make_pair(true, image);
 }
