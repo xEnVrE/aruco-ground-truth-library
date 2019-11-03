da954119757b483b6e8226a0fe04759726631ede
Nicola Piga <nicolapiga@gmail.com>
2019-11-03 19:03:43 +0100

    Update TestVisualization

--- src/ground-truth-module/src/TestVisualization.cpp
+++ src/ground-truth-module/src/TestVisualization.cpp
@@ -9,13 +9,26 @@
 #include <VtkContent.h>
 #include <VtkiCubHand.h>
 
-int main()
+#include <cstdlib>
+
+int main(int argc, char** argv)
 {
+    if (argc != 3)
+    {
+        std::cout << "Synopsis: test-visualization <robot_name> <use_analogs>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::string robot_name = std::string(argv[1]);
+    bool use_analogs = false;
+    if (std::string(argv[2]) == "true")
+        use_analogs = true;
+
+
     VtkContainer container(30, 600, 600);
 
     std::unique_ptr<VtkContent> hand = std::unique_ptr<VtkiCubHand>
     (
-        new VtkiCubHand("test-visualization", "left")
+        new VtkiCubHand(robot_name, "left", "test-visualization", use_analogs)
     );
     container.add_content("hand", std::move(hand));
     container.run();
