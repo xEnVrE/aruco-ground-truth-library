7c3ccd5f987ac06c0cda898cdfa54f0cd695a05e
Nicola Piga <nicolapiga@gmail.com>
2019-11-03 14:52:27 +0100

    Implement generic VTK content within class VtkContent

---
+++ src/viz-library/include/VtkContent.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef VTKCONTENT_H
+#define VTKCONTENT_H
+
+#include <vtkRenderer.h>
+
+class VtkContent
+{
+public:
+    virtual ~VtkContent();
+
+    virtual void add_to_renderer(vtkRenderer& renderer) = 0;
+
+    virtual bool update_pose(const bool& blocking) = 0;
+
+private:
+    const std::string log_name_ = "VtkContent";
+};
+
+#endif /* VTKCONTENT_H */
