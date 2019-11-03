acb7f01057ecf53a6fb507ef8d97b9dc5b773691
Nicola Piga <nicolapiga@gmail.com>
2019-11-03 14:56:15 +0100

    Implement iCubHand as a VtkContent

---
+++ src/viz-library/include/VtkiCubHand.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef VTKICUBHAND_H
+#define VTKICUBHAND_H
+
+#include <Eigen/Dense>
+
+#include <iCubForwardKinematics.h>
+#include <VtkContent.h>
+#include <VtkMeshOBJ.h>
+
+#include <memory>
+#include <unordered_map>
+
+#include <vtkRenderer.h>
+
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Network.h>
+#include <yarp/sig/Vector.h>
+
+
+class VtkiCubHand : public VtkContent
+{
+public:
+    VtkiCubHand(const std::string port_prefix, const std::string laterality);
+
+    virtual ~VtkiCubHand();
+
+    void add_to_renderer(vtkRenderer& renderer) override;
+
+    bool update_pose(const bool& blocking) override;
+
+private:
+    yarp::os::Network yarp_;
+
+    std::unordered_map<std::string, VtkMeshOBJ> meshes_;
+
+    yarp::os::BufferedPort<yarp::sig::Vector> hand_pose_port_in_;
+
+    // iCubArmModel hand_model_;
+
+    std::unique_ptr<iCubForwardKinematics> forward_kinematics_;
+
+    const std::string log_name_ = "VtkiCubHand";
+};
+
+#endif /* VTKICUBHAND_H */
---
+++ src/viz-library/src/VtkiCubHand.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <VtkiCubHand.h>
+
+#include <yarp/eigen/Eigen.h>
+#include <stdexcept>
+
+using namespace Eigen;
+using namespace yarp::eigen;
+
+
+VtkiCubHand::VtkiCubHand(const std::string port_prefix, const std::string laterality)
+{
+    if ((laterality != "left") && (laterality != "right"))
+        throw(std::runtime_error(log_name_ + "::ctor. Invalid laterality specified."));
+
+    std::string laterality_key = ((laterality == "right") ? "Right" : "Left");
+
+    /* Check YARP network. */
+    if (!yarp_.checkNetwork())
+    {
+        throw(std::runtime_error(log_name_ + "::ctor. Error: YARP network is not available."));
+    }
+
+    /* Open input port. */
+    if(!hand_pose_port_in_.open("/" + port_prefix + "/icub/hand/" + laterality + ":i"))
+        throw(std::runtime_error(log_name_ + "::ctor. Cannot open hand pose input port."));
+
+    /* Add meshes of hand parts. */
+    meshes_.emplace("palm", VtkMeshOBJ("full_" +  laterality_key + "HandPalm_red.obj", true));
+
+    meshes_.emplace("thumb0", VtkMeshOBJ("full_" + laterality_key + "Thumb0.obj", true));
+    meshes_.emplace("thumb1", VtkMeshOBJ("full_" + laterality_key + "ThumbFinger1.obj", true));
+    meshes_.emplace("thumb2", VtkMeshOBJ("full_" + laterality_key + "ThumbFinger2.obj", true));
+    meshes_.emplace("thumb3", VtkMeshOBJ("full_" + laterality_key + "ThumbFingerTip.obj", true));
+
+    meshes_.emplace("index0", VtkMeshOBJ("full_" + laterality_key + "IndexFinger0.obj", true));
+    meshes_.emplace("index1", VtkMeshOBJ("full_" + laterality_key + "IndexFinger1.obj", true));
+    meshes_.emplace("index2", VtkMeshOBJ("full_" + laterality_key + "IndexFinger2.obj", true));
+    meshes_.emplace("index3", VtkMeshOBJ("full_" + laterality_key + "IndexFingerTip_b.obj", true));
+
+    meshes_.emplace("middle0", VtkMeshOBJ("full_" + laterality_key + "MiddleFinger0.obj", true));
+    meshes_.emplace("middle1", VtkMeshOBJ("full_" + laterality_key + "MiddleFinger1.obj", true));
+    meshes_.emplace("middle2", VtkMeshOBJ("full_" + laterality_key + "MiddleFinger2.obj", true));
+    meshes_.emplace("middle3", VtkMeshOBJ("full_" + laterality_key + "MiddleFingerTip_b.obj", true));
+
+    meshes_.emplace("ring0", VtkMeshOBJ("full_" + laterality_key + "RingFinger0.obj", true));
+    meshes_.emplace("ring1", VtkMeshOBJ("full_" + laterality_key + "RingFinger1.obj", true));
+    meshes_.emplace("ring2", VtkMeshOBJ("full_" + laterality_key + "RingFinger2.obj", true));
+    meshes_.emplace("ring3", VtkMeshOBJ("full_" + laterality_key + "RingFingerTip_b.obj", true));
+
+    meshes_.emplace("little0", VtkMeshOBJ("full_" + laterality_key + "LittleFinger0.obj", true));
+    meshes_.emplace("little1", VtkMeshOBJ("full_" + laterality_key + "LittleFinger1.obj", true));
+    meshes_.emplace("little2", VtkMeshOBJ("full_" + laterality_key + "LittleFinger2.obj", true));
+    meshes_.emplace("little3", VtkMeshOBJ("full_" + laterality_key + "LittleFingerTip_b.obj", true));
+
+    /* Configure forward kinematics. */
+    forward_kinematics_ = std::unique_ptr<iCubForwardKinematics>
+    (
+        new iCubForwardKinematics(laterality_key + "Hand")
+    );
+}
+
+
+VtkiCubHand::~VtkiCubHand()
+{
+    hand_pose_port_in_.close();
+}
+
+
+void VtkiCubHand::add_to_renderer(vtkRenderer& renderer)
+{
+    /* Add all the parts to the renderer. */
+    for (auto mesh : meshes_)
+        mesh.second.add_to_renderer(renderer);
+}
+
+
+bool VtkiCubHand::update_pose(const bool& blocking)
+{
+    yarp::sig::Vector* pose_in = hand_pose_port_in_.read(blocking);
+
+    if (pose_in == nullptr)
+        return false;
+
+    VectorXd pose = toEigen(*pose_in);
+    Transform<double, 3, Affine> transform;
+    transform = Translation<double, 3>(pose.head<3>());
+    transform.rotate(AngleAxisd(pose(6), pose.segment<3>(3)));
+
+    /* Palm. */
+    for (auto mesh : meshes_)
+        mesh.second.set_pose(transform * forward_kinematics_->map("ee", mesh.first));
+
+    return true;
+}
