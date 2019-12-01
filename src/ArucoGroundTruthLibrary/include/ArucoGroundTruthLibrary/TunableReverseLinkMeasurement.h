6267f6e53a6c47b605c8869793032539a10069f3
Nicola Piga <nicolapiga@gmail.com>
2019-12-01 16:16:40 +0100

    Implement class TunableReverseLinkMeasurement

---
+++ src/ArucoGroundTruthLibrary/include/ArucoGroundTruthLibrary/TunableReverseLinkMeasurement.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef ARUCOGROUNDTRUTHLIBRARY_TUNABLEREVERSELINKMEASUREMENT_H
+#define ARUCOGROUNDTRUTHLIBRARY_TUNABLEREVERSELINKMEASUREMENT_H
+
+#include <ArucoGroundTruthLibrary/ArucoMeasurement.h>
+#include <ArucoGroundTruthLibrary/ReverseLinkMeasurement.h>
+
+#include <Eigen/Dense>
+
+#include <memory>
+#include <string>
+
+namespace ArucoGroundTruthLibrary{
+    class TunableReverseLinkMeasurement;
+}
+
+
+class ArucoGroundTruthLibrary::TunableReverseLinkMeasurement : public ArucoGroundTruthLibrary::ReverseLinkMeasurement
+{
+public:
+    TunableReverseLinkMeasurement(Eigen::Transform<double, 3, Eigen::Affine> transform_0, std::unique_ptr<ArucoGroundTruthLibrary::ArucoMeasurement> aruco_measurement);
+
+    virtual ~TunableReverseLinkMeasurement();
+
+    virtual void reset();
+
+    virtual void shift_x(const double& length);
+
+    virtual void shift_y(const double& length);
+
+    virtual void shift_z(const double& length);
+
+    virtual void rotate_x(const double& angle);
+
+    virtual void rotate_y(const double& angle);
+
+    virtual void rotate_z(const double& angle);
+
+protected:
+    Eigen::Transform<double, 3, Eigen::Affine> evaluate_reverse_transform() override;
+
+private:
+
+    const Eigen::Transform<double, 3, Eigen::Affine> transform_0_;
+
+    Eigen::Transform<double, 3, Eigen::Affine> current_transform_;
+
+    const std::string log_name_ = "TunableReverseLinkMeasurement";
+};
+
+#endif /* ARUCOGROUNDTRUTHLIBRARY_REVERSELINKMEASUREMENT_H */
---
+++ src/ArucoGroundTruthLibrary/src/TunableReverseLinkMeasurement.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2019 Istituto Italiano di Tecnologia (IIT)
+ *
+ * This software may be modified and distributed under the terms of the
+ * GPL-2+ license. See the accompanying LICENSE file for details.
+ */
+
+#include <ArucoGroundTruthLibrary/TunableReverseLinkMeasurement.h>
+#include "ArucoGroundTruthLibrary/ReverseLinkMeasurement.h"
+
+using namespace ArucoGroundTruthLibrary;
+using namespace Eigen;
+
+
+TunableReverseLinkMeasurement::TunableReverseLinkMeasurement(Eigen::Transform<double, 3, Eigen::Affine> transform_0, std::unique_ptr<ArucoMeasurement> aruco_measurement) :
+    ReverseLinkMeasurement(std::move(aruco_measurement)),
+    transform_0_(transform_0),
+    current_transform_(transform_0)
+{
+    ReverseLinkMeasurement::initialize();
+}
+
+
+TunableReverseLinkMeasurement::~TunableReverseLinkMeasurement()
+{}
+
+
+void TunableReverseLinkMeasurement::reset()
+{
+    current_transform_ = transform_0_;
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::shift_x(const double& length)
+{
+    current_transform_ *= Translation<double, 3>(length, 0.0, 0.0);
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::shift_y(const double& length)
+{
+    current_transform_ *= Translation<double, 3>(0.0, length, 0.0);
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::shift_z(const double& length)
+{
+    current_transform_ *= Translation<double, 3>(0.0, 0.0, length);
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::rotate_x(const double& angle)
+{
+    current_transform_ *= AngleAxisd(angle, Vector3d::UnitX());
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::rotate_y(const double& angle)
+{
+    current_transform_ *= AngleAxisd(angle, Vector3d::UnitY());
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+void TunableReverseLinkMeasurement::rotate_z(const double& angle)
+{
+    current_transform_ *= AngleAxisd(angle, Vector3d::UnitZ());
+
+    ReverseLinkMeasurement::initialize();
+}
+
+
+Eigen::Transform<double, 3, Eigen::Affine> TunableReverseLinkMeasurement::evaluate_reverse_transform()
+{
+    return current_transform_;
+}
